5f1aaf98fd93c579556b074dac8ad14cca01e535
Casey Baker <caseye.baker@gmail.com>
2019-01-23 15:43:31 -0800

    test URL

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,3 +3,6 @@ const router = express.Router();
 
 //Require controllers (to be created)
 const product_controller = require('../controllers/product.controller');
+
+//Test URL for file communication
+router.get('/test', product_controller.test);
