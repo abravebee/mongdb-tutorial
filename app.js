c8180383dba27f53908484427d9b261c770f2285
Casey Baker <caseye.baker@gmail.com>
2019-01-23 15:48:24 -0800

    import routes for products

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//Import routes for products
+const product = require('./routes/product.route');
 // initialize our express app
 const app = express();
 
