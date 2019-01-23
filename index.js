e369db517f858b38bb55b1f7f0eeec9862c1c235
Casey Baker <caseye.baker@gmail.com>
2019-01-23 13:01:32 -0800

    fixes typo

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var db = MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db)
   if(err)
     throw err;
     console.log("connected to the mongoDB");
-    myCollect = db.collection('test_collection');
+    myCollection = db.collection('test_collection');
 })
