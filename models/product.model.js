be062cc095849911c096a8c2d54be30c88c662a9
Casey Baker <caseye.baker@gmail.com>
2019-01-23 15:39:54 -0800

    exports model

--- models/product.model.js
+++ models/product.model.js
@@ -5,3 +5,6 @@ let ProductSchema = new Schema({
   name: {type: String, required: true, max: 100},
   price: {type: Number, required: true},
 })
+
+//Export the model
+module.exports = mongoose.model('Product', ProductSchema);
