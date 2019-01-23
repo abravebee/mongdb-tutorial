bb12a595bb8de560e685d03e586bc1edba834e51
Casey Baker <caseye.baker@gmail.com>
2019-01-23 15:42:49 -0800

    require product controller

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
+//Require controllers (to be created)
+const product_controller = require('../controllers/product.controller');
