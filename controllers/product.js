be72125bbc7d021b5e4b6087cd1263c81d2dd518
Casey Baker <caseye.baker@gmail.com>
2019-01-24 13:17:24 -0800

    adds controller for production create

--- controllers/product.js
+++ controllers/product.js
@@ -5,3 +5,19 @@ exports.test = function (req, res) {
   res.send('Greetings from the Test controller!');
   console.log('Test controller');
 }
+
+exports.product_create = function (req, res) {
+  let product = new Product(
+    {
+      name: req.body.name,
+      price: req.body.price
+    }
+  );
+
+  product.save(function (err) {
+    if(err) {
+      return next(err);
+    }
+    res.send('Product created successfully')
+  })
+}
