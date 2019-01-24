6e379fced14d2ae457253f58f2099bbfe5b98ec8
Casey Baker <caseye.baker@gmail.com>
2019-01-24 12:54:25 -0800

    require mongoose; define dev_db_url

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const bodyParser = require('body-parser');
 //Import routes for products
 const product = require('./routes/product');
 
-// initialize our express app
+//Initialize our express app
 const app = express();
 app.use('/products', product);
 
+//Connecting app to the remote database
+//Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:abcd1234@ds211625.mlab.com:11625/productstutorial';
+
+//Port
 let port = 5500;
 
 app.listen(port, () => {
