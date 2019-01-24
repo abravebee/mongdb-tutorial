ce56c5d56fbc663523d248ebe89b49d734d4b134
Casey Baker <caseye.baker@gmail.com>
2019-01-24 13:39:01 -0800

    moves useNewUrlParser fix to mongoose.connect from dev_db_url

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const app = express();
 //Connecting app to the remote database
 //Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = ('mongodb://someuser:abcd1234@ds211625.mlab.com:11625/productstutorial', { useNewUrlParser: true });
+let dev_db_url = 'mongodb://someuser:abcd1234@ds211625.mlab.com:11625/productstutorial';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
