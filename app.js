dbb79b37fd1e2eda8a806b06f79ae4982923739d
Casey Baker <caseye.baker@gmail.com>
2019-01-24 12:59:45 -0800

    implement body parser

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+//Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 //Port
 let port = 5500;
 
