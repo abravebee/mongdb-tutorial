fef4662e187ad39473dfd7f7678906a2460005ca
Casey Baker <caseye.baker@gmail.com>
2019-01-24 12:56:06 -0800

    define mongoDB env; connect to mongoose; mongoose Promise

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use('/products', product);
 //Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://someuser:abcd1234@ds211625.mlab.com:11625/productstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
 
 //Port
 let port = 5500;
