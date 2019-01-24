ed9f437fa6dd76cc4805a155a710200f459ea584
Casey Baker <caseye.baker@gmail.com>
2019-01-24 14:13:45 -0800

    adds delete controller

--- controllers/product.js
+++ controllers/product.js
@@ -35,3 +35,10 @@ exports.product_update = function (req, res) {
       res.send('Product updated.');
     });
 };
+
+exports.product_delete = function (req, res) {
+  Product.findByIdAndRemove(req.params.id, function (err) {
+    if(err) return next(err);
+    res.send('Deleted successfully!');
+  })
+}
